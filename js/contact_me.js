4a07ceafd28e3905ad6df6250ce00434b922f1ce
Erik Tufvesson <erik.tufvesson@gmail.com>
2017-08-31 21:18:25 +0200

    anmälan

--- js/contact_me.js
+++ js/contact_me.js
@@ -2,6 +2,61 @@
 
 $(function() {
 
+    $("#anmalanForm input,#anmalanForm textarea").jqBootstrapValidation({
+        preventSubmit: true,
+        submitError: function($form, event, errors) {
+            // additional error messages or events
+        },
+        submitSuccess: function($form, event) {
+            event.preventDefault(); // prevent default submit behaviour
+            // get values from FORM
+            var name = $("#anmalanForm input#name").val();
+            var email = $("#anmalanForm input#email").val();
+            var firstName = name; // For Success/Failure Message
+            // Check for white space in name for Success/Fail message
+            if (firstName.indexOf(' ') >= 0) {
+                firstName = name.split(' ').slice(0, -1).join(' ');
+            }
+            $.ajax({
+                url: "../mail/anmalan.php?date=2017-10-05&time=18:00",
+                type: "POST",
+                data: {
+                    name: name,
+                    email: email
+                },
+                cache: false,
+                success: function() {
+                    // Success message
+                    $('#anmalanForm #success').html("<div class='alert alert-success'>");
+                    $('#anmalanForm #success > .alert-success')
+                        .append("<strong>Tack för ditt intresse! Guiden kommer skickas till din e-postadress.</strong>");
+                    $('#anmalanForm #success > .alert-success')
+                        .append('</div>');
+
+                    //clear all fields
+                    $('#anmalanForm').trigger("reset");
+
+                    //hide submit button and show close button
+                    $('#anmalanForm button[type=submit]').hide();
+                    $('#anmalanForm button#close').show(); 
+                },
+                error: function(err) {
+                    console.log(err);
+
+                    // Fail message
+                    $('#anmalanForm #success').html("<div class='alert alert-danger'>");
+                    $('#anmalanForm #success > .alert-danger').append($("<strong>").text("Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!"));
+                    $('#anmalanForm #success > .alert-danger').append('</div>');
+                    // clear all fields
+                    // $('#bookingForm').trigger("reset");
+                },
+            });
+        },
+        filter: function() {
+            return $(this).is(":visible");
+        },
+    });
+
     $("#garderobenForm input,#garderobenForm textarea").jqBootstrapValidation({
         preventSubmit: true,
         submitError: function($form, event, errors) {
@@ -34,19 +89,19 @@ $(function() {
                         .append('</div>');
 
                     //clear all fields
-                    $('#bookingForm').trigger("reset");
+                    $('#garderobenForm').trigger("reset");
 
                     //hide submit button and show close button
-                    $('#bookingForm button[type=submit]').hide();
-                    $('#bookingForm button#close').show(); 
+                    $('#garderobenForm button[type=submit]').hide();
+                    $('#garderobenForm button#close').show(); 
                 },
                 error: function(err) {
                     console.log(err);
 
                     // Fail message
-                    $('#bookingForm #success').html("<div class='alert alert-danger'>");
-                    $('#bookingForm #success > .alert-danger').append($("<strong>").text("Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!"));
-                    $('#bookingForm #success > .alert-danger').append('</div>');
+                    $('#garderobenForm #success').html("<div class='alert alert-danger'>");
+                    $('#garderobenForm #success > .alert-danger').append($("<strong>").text("Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!"));
+                    $('#garderobenForm #success > .alert-danger').append('</div>');
                     // clear all fields
                     // $('#bookingForm').trigger("reset");
                 },
